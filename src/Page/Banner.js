c68b3f9d6ff42e7e542c91ade9bc1da213cca872
Shakhawathh <shakhawathh55@gmail.com>
2023-02-04 14:45:15 +0600

    adding sector

--- src/Page/Banner.js
+++ src/Page/Banner.js
@@ -1,121 +1,149 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Banner = () => {
+  const data = [{}];
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <div>
-      <div class="w-full bg-white bg-[url('https://i.ibb.co/X725PYT/ian-harber-cu7-Kou-Q5-FJE-unsplash.jpg')] bg-no-repeat bg-center bg-cover  ">
-        <div class="relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-24">
-          <div class="flex w-full mx-auto text-left">
-            <div class="relative inline-flex items-center mx-auto align-middle">
-              <div class="pb-12 text-center">
-                <h1 class="max-w-5xl text-2xl font-bold leading-none tracking-tighter text-teal-600 md:text-5xl lg:text-6xl lg:max-w-7xl">
-                  To do list <br class="hidden lg:block" />
-                  for your daily life
-                </h1>
+      <div class="w-full bg-white  ">
+        <section>
+          <div class="px-4 py-12 mx-auto max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24">
+            <div class="flex flex-wrap items-center mx-auto max-w-7xl">
+              <div class="w-full lg:max-w-lg lg:w-1/2 rounded-xl">
+                <div>
+                  <div class="relative w-full max-w-lg">
+                    <div class="absolute top-0 rounded-full bg-violet-300 -left-4 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
 
-                <form
-                  action=""
-                  method="post"
-                  id="revue-form"
-                  name="revue-form"
-                  target="_blank"
-                  class="p-2 mt-8 transition duration-500 ease-in-out transform border2 bg-gray-50 md:mx-auto rounded-xl sm:max-w-lg sm:flex"
-                >
-                  <div class="flex-1 min-w-0 revue-form-group">
-                    <label for="member_email" class="sr-only">
-                      Email address
-                    </label>
-                    <input
-                      id="cta-email"
-                      type="email"
-                      class="block w-full px-5 py-3 text-base placeholder-gray-500 transition duration-500 ease-in-out transform bg-transparent border border-transparent rounded-md text-neutral-600 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
-                      placeholder="Enter your email  "
-                    />
+                    <div class="absolute rounded-full bg-fuchsia-300 -bottom-24 right-20 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+                    <div class="relative">
+                      <div className="bg-green-500">
+                        <form onSubmit={handleSubmit(onSubmit)}>
+                          <div className="card-body px-6 py-3">
+                            <div className="form-control">
+                              <label className="input-group input-group-sm pt-2">
+                                <span className="bg-info text-white ">
+                                  Your Name
+                                </span>
+                                <input
+                                  {...register(`name`)}
+                                  className="input bg-base-300 input-sm w-full bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg"
+                                  placeholder="Your name"
+                                  type="link"
+                                  required
+                                />
+                              </label>
+                            </div>
+                            <div className="form-control">
+                              <label className="input-group input-group-sm pt-2">
+                                <span className="bg-info text-white ">
+                                  Teacher
+                                </span>
+                                <input
+                                  {...register(`sectors`)}
+                                  type="text"
+                                  placeholder="Teacher name"
+                                  class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-8 px-4 block w-full appearance-none leading-normal"
+                                  required
+                                />
+                              </label>
+                            </div>
+                            <div className="form-control">
+                              <label className="input-group input-group-sm pt-2">
+                                <span className="bg-info text-white">
+                                  Agree to terms
+                                </span>
+
+                                <input
+                                  {...register(`check`)}
+                                  type="checkbox"
+                                  placeholder="Subject here"
+                                  class="checkbox"
+                                  required
+                                />
+                              </label>
+                            </div>
+                            <div className="flex flex-col lg:flex-row md:flex-row justify-between gap-2  pt-2"></div>
+
+                            <div className="card-actions justify-end pt-3">
+                              <button
+                                type="submit"
+                                className=" btn font-thin btn-sm btn-info text-white hover:bg-green-600"
+                              >
+                                <i className="fa-solid fa-paper-plane mr-1"></i>
+                                Post
+                              </button>
+                            </div>
+                          </div>
+                        </form>
+                      </div>
+                    </div>
                   </div>
-                  <div class="mt-4 sm:mt-0 sm:ml-3 revue-form-actions">
-                    <button
-                      type="submit"
-                      value="Subscribe"
-                      name="member[subscribe]"
-                      id="member_submit"
-                      class="block w-full px-5 py-3 text-base font-medium text-white bg-teal-700 border border-transparent rounded-lg shadow hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 sm:px-10"
-                    >
-                      Notify me
-                    </button>
+                </div>
+              </div>
+              <div class="flex flex-col items-start mt-12 mb-16 text-left lg:flex-grow lg:w-1/2 lg:pl-6 xl:pl-24 md:mb-0 xl:mt-0">
+                <span class="mb-8 text-xs font-bold tracking-widest text-blue-600 uppercase">
+                  {" "}
+                  Your tagline{" "}
+                </span>
+                <h1 class="mb-8 text-4xl font-bold leading-none tracking-tighter text-neutral-600 md:text-7xl lg:text-5xl">
+                  Medium length display headline.
+                </h1>
+                <p class="mb-8 text-base leading-relaxed text-left text-gray-500">
+                  Free and Premium themes, UI Kit's, templates and landing pages
+                  built with Tailwind CSS, HTML &amp; Next.js.
+                </p>
+                <div class="flex-col mt-0 lg:mt-6 max-w-7xl sm:flex">
+                  <form
+                    action=""
+                    method="post"
+                    id="revue-form"
+                    name="revue-form"
+                    target="_blank"
+                    class="p-2 mt-8 transition duration-500 ease-in-out transform border2 bg-gray-50 rounded-xl sm:max-w-lg sm:flex"
+                  >
+                    <div class="flex-1 min-w-0 revue-form-group">
+                      <label for="member_email" class="sr-only">
+                        Email address
+                      </label>
+                      <input
+                        id="cta-email"
+                        type="email"
+                        class="block w-full px-5 py-3 text-base placeholder-gray-300 transition duration-500 ease-in-out transform bg-transparent border border-transparent rounded-md text-neutral-600 focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300"
+                        placeholder="Enter your email  "
+                      />
+                    </div>
+                    <div class="mt-4 sm:mt-0 sm:ml-3 revue-form-actions">
+                      <button
+                        type="submit"
+                        value="Subscribe"
+                        name="member[subscribe]"
+                        id="member_submit"
+                        class="block w-full px-5 py-3 text-base font-medium text-white bg-blue-600 border border-transparent rounded-lg shadow hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 sm:px-10"
+                      >
+                        Notify me
+                      </button>
+                    </div>
+                  </form>
+                  <div class="sm:max-w-lg sm:flex">
+                    <p class="mt-3 text-xs text-gray-500">
+                      By subscribing, you agree with Revue’s
+                      <a target="_blank" href="https://www.getrevue.co/terms">
+                        Terms of Service
+                      </a>
+                      and
+                      <a target="_blank" href="https://www.getrevue.co/privacy">
+                        Privacy Policy
+                      </a>
+                      .
+                    </p>
                   </div>
-                </form>
-                <div class="sm:max-w-lg sm:flex md:mx-auto">
-                  <p class="mt-3 text-xs text-gray-500">
-                    By subscribing, you agree with Revue’s
-                    <a target="_blank" href="https://www.getrevue.co/terms">
-                      Terms of Service
-                    </a>
-                    and
-                    <a target="_blank" href="https://www.getrevue.co/privacy">
-                      Privacy Policy
-                    </a>
-                    .
-                  </p>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="bg-neutral-50">
-        <div class=" px-5 py-12 mx-auto lg:px-16">
-          <div class="flex flex-col w-full mb-8 text-center">
-            <span class="mb-4 text-sm font-medium tracking-wide text-gray-500 uppercase">
-              The best front end teams use Wickedtemplates to state their
-              presence.
-              <a
-                href="https://www.wickedtemplates.com/expo.html"
-                class="font-semibold text-blue-600 lg:mb-0 hover:text-blue-500"
-              >
-                Our customers »
-              </a>
-            </span>
-          </div>
-          <div class="mx-auto text-center">
-            <div class="grid grid-cols-5 gap-4 mx-auto lg:grid-cols-5">
-              <div>
-                <img
-                  class="h-4 mx-auto lg:h-12"
-                  src="https://d33wubrfki0l68.cloudfront.net/5a364f2e7cfeadd0f603cdfeda83f5cd0509770d/3f0ae/images/logos/logoone.svg"
-                  alt="Figma"
-                />
-              </div>
-              <div>
-                <img
-                  class="h-4 mx-auto lg:h-12"
-                  src="https://d33wubrfki0l68.cloudfront.net/ab0d1eeefb9cddb55f05f1601b2ae3fbae9317a9/5bfbe/images/logos/logotwo.svg"
-                  alt="Framer"
-                />
-              </div>
-              <div>
-                <img
-                  class="h-4 mx-auto lg:h-12"
-                  src="https://d33wubrfki0l68.cloudfront.net/2fea2d550675d7cf3bb77a515487bce6c086051b/951f5/images/logos/logothree.svg"
-                  alt="Sketch "
-                />
-              </div>
-              <div>
-                <img
-                  class="h-4 mx-auto lg:h-12"
-                  src="https://d33wubrfki0l68.cloudfront.net/f9b8da8b1442382848d30275dc2d0337d14a04c9/dc8f4/images/logos/logofour.svg"
-                  alt="Sketch "
-                />
-              </div>
-              <div>
-                <img
-                  class="h-4 mx-auto lg:h-12"
-                  src="https://d33wubrfki0l68.cloudfront.net/07ddf740e29509004147c6a83c09f299366546c9/03a26/images/logos/logofive.svg"
-                  alt="Invision"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+        </section>
       </div>
     </div>
   );
